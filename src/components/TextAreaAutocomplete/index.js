1569b690b9d572d4e484254f167ab99b91361410
Maxwel <maxwelsant96@icloud.com>
2021-12-03 08:49:48 -0300

    init project

---
+++ src/components/TextAreaAutocomplete/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+
+const TextAreaAutocomplete = () => {
+  return <textarea className="TextAreaAutocomplete-textarea"></textarea>;
+};
+
+export { TextAreaAutocomplete };
